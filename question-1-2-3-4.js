b164383d798bfdbaad244ea86878bc189663395a
PSC Student <psc@billkuker.com>
2022-09-26 11:20:29 -0400

    arrays prac

--- question-1-2-3-4.js
+++ question-1-2-3-4.js
@@ -3,27 +3,32 @@
 //Below this line, fill in the array named "assignments" with the string values
 //from question one in the lab.
 
-let assignments = [        ];
+let assignments = [ "Lab1", "Lab2", "Mid Term", "Lab3","Project", "Final Exam" ];
 
 
 //❓❓ LAB QUESTION 2:
 //Below this line, declare an array named "grades" with the numeric values
 //from question one in the lab.
 
+let grades = [90, 80, 78, 81, 91, 89];
 
 
 
-//❓❓ LAB QUESTION 3:
+// LAB QUESTION 3:
 //Below this line, declare an array named "grades" with the numeric values
 //from question one in the lab.
 console.log("Grades:");
 for ( let i = 0; i < assignments.length; i++ ){
-    console.log(assignments[i], "????");
+    console.log(assignments[i], grades[i]);
 }
 
 
 //❓❓ LAB QUESTION 4:
 //Calculate the average grade and print it out:
-
+let sum = 0;
+for (let j = 0; j < grades.length; j++){
+    sum = sum + grades[j]
+}
+console.log("Average Grade:", sum/ grades.length);
 
 
